ab8a058a0befa9eaf4b43494dcfcac8632dd0d1f
MbarekAmehry <mbarek.amehry@gmail.com>
2021-11-09 11:09:50 +0100

    compile to css to avoid confusion

--- samples/vue-basics-project/components/PostsList.js
+++ samples/vue-basics-project/components/PostsList.js
@@ -1,23 +1,21 @@
 app.component('posts-list', {
-  template: ` <div id="posts" class="lg:w-2/3 px-3 mt-3 lg:mt-0 flex flex-wrap">
-  <div v-for="post in posts" :key="post.id" class="lg:w-10/12 rounded border border-gray-200 px-2 py-1 mb-2">
-      <div class="flex relative">
-          <img :src="post.img" :alt="post.title" class="w-32 rounded shadow mr-2 ">
-          <div>
-              <h3 class="text-gray-900 text-xl mb-1 font-semibold">{{post.title}}</h3>
-              <p class="text-gray-800">{{post.body}} </p>
-          </div>
-          <div class="actions flex absolute right-1 top-0 space-x-1 pt-1">
-              <div @click="$emit('on-edit',post.id)"
-                  class="flex items-center justify-center w-6 h-6 text-blue-500 rounded hover:bg-blue-100">
-                  <i class="fa fa-edit text-sm"></i>
-              </div>
-              <div @click="$emit('on-delete',post.id)"
-                  class="flex items-center justify-center w-6 h-6 text-red-500 rounded hover:bg-red-100">
-                  <i class="fa fa-trash text-sm"></i>
-              </div>
-          </div>
+  template: `<div id="posts" class="posts-list">
+  <div v-for="post in posts" :key="post.id" class="post-single">
+    <img :src="post.img" :alt="post.title" />
+    <div>
+      <h3>
+        {{ post.title }}
+      </h3>
+      <p>{{ post.body }}</p>
+    </div>
+    <div class="actions">
+      <div @click="$emit('on-edit', post.id)" class="edit">
+        <i class="fa fa-edit"></i>
       </div>
+      <div @click="$emit('on-delete', post.id)" class="delete">
+        <i class="fa fa-trash"></i>
+      </div>
+    </div>
   </div>
 </div>`,
   props: ['posts'],
--- samples/vue-basics-project/components/UserCard.js
+++ samples/vue-basics-project/components/UserCard.js
@@ -1,34 +1,27 @@
 app.component('user-card', {
-  template: `<div class="
-    w-60
-    mx-auto
-    text-center
-    rounded-lg
-    py-4
-    border border-gray-300
-  ">
-    <div class="relative inline">
-        <img v-bind:src="user.avatar" alt=""
-            class="w-24 h-24 mx-auto border-4 rounded-full mb-3" :class="user.gender == 'female'? 'border-pink-600': 'border-indigo-300'"/>
-    </div>
-    <div class="text-green-600" v-if="experince<1">syagaire</div>
-    <div class="text-gray-600" v-else-if="experince<5">junior</div>
-    <div class="text-gray-600" v-else>senior</div>
-    <p class="text-gray-700 font-semibold cursor-pointer hover:underline">
-        @{{user.username}}
-    </p>
-    <span class="text-sm mr-2 bg-gray-200 p-1 rounded-md" v-for="tag in user.tags">{{tag}}</span>
+  template: ` <div class="user-card">
+  <div class="avatar">
+    <img
+      v-bind:src="user.avatar"
+      :alt="user.username"
+      :class="
+        user.gender == 'female' ? 'border-pink-600' : 'border-indigo-300'
+      "
+    />
+  </div>
+  <div class="experience" v-if="experince < 1">syagaire</div>
+  <div class="experience" v-else-if="experince < 5">junior</div>
+  <div class="experience" v-else>senior</div>
+  <p class="username">@{{ user.username }}</p>
+  <span class="job">{{ user.job }}</span>
+  <span class="tags" v-for="tag in user.tags" :key="tag">{{ tag }}</span>
 
-    <span class="block text-gray-500 text-xs mt-1">{{user.job}}</span>
-    <button class="mt-3
-        px-4
-        py-1
-        rounded
-        text-white" :class="following?
-        'bg-indigo-300':
-        'bg-indigo-600'" @click="followUser">
-        {{following? 'following': 'follow'}}
-    </button>
+  <button
+    :class="following ? 'bg-indigo-400' : 'bg-indigo-500'"
+    @click="followUser"
+  >
+    {{ following ? "following" : "follow" }}
+  </button>
 </div>`,
   data() {
     return {
