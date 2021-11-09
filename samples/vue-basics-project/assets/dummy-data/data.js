9e5fe93be7b9d565d4aeb964763a91fbfa680754
MbarekAmehry <mbarek.amehry@gmail.com>
2021-11-09 11:09:21 +0100

    add data samples

--- samples/vue-basics-project/assets/dummy-data/data.js
+++ samples/vue-basics-project/assets/dummy-data/data.js
@@ -10,4 +10,31 @@ const user = {
   status: true,
 };
 
+const posts = [
+  {
+    id: 1,
+    title: 'Post One',
+    body: 'amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, quaerat?',
+    img: 'https://source.unsplash.com/600x300/?nature,water',
+  },
+  {
+    id: 2,
+    title: 'Post Two',
+    body: 'amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, quaerat?',
+    img: 'https://source.unsplash.com/600x300/?space,stars',
+  },
+  {
+    id: 3,
+    title: 'Post Three',
+    body: 'amet consectetur adipisicing elit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, quaerat?',
+    img: 'https://source.unsplash.com/600x300/?dev,code',
+  },
+],
+
+const post = {
+  title: '',
+  body: '',
+  img: 'https://source.unsplash.com/600x300/?',
+},
+
 const api = 'https://randomuser.me/api/?results=8';
